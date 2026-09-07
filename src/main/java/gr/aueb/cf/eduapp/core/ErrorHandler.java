18512d1c11a05919f39c71992918a057196a6312
a8anassis <a8anassis@gmail.com>
2026-09-07 17:49:51 +0300

    chore: change ErrorHandler to use RestControllerAdvice for improved exception handling

--- src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
@@ -15,12 +15,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
